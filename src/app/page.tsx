57cb9a2589ae7279c231653632a8a2dd6ea00b2d
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-03 21:36:25 -0700

    Profile image centered on small screen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,24 @@ export default function Home() {
   return (
     <div className="flex justify-center h-screen items-center bg-slate-800">
       <div className="flex flex-col items-center">
-        <div className="md:flex justify-center items-center m-4 w-max">
-          <div className="h-32 w-32 overflow-hidden rounded-full">
-            <Image
-              alt="MB Headshot"
-              height={10000}
-              width={10000}
-              src="/headshot.jpg"
-              className="rounded-full scale-125 flex justify-center"
-            />
+        <div className="md:flex md:justify-center m-4 w-max">
+          <div className="flex justify-center">
+            <div className="h-32 w-32 overflow-hidden rounded-full">
+              <Image
+                alt="MB Headshot"
+                height={10000}
+                width={10000}
+                src="/headshot.jpg"
+                className="rounded-full scale-125"
+              />
+            </div>
           </div>
-          <div className="w-screen px-4 font-mono tracking-tight flex flex-col justify-center md:w-96">
-            <h1 className="text-xl py-2">Hi! My name is MelonBred.</h1>
+          <div className="w-96 px-4 font-mono tracking-tight flex flex-col justify-center text-center md:w-96 md:text-left">
+            <h1 className="text-xl py-2">Hi! My name is Cliff.</h1>
             <p className="text-sm">
-              I&apos;m a mechanical keyboard hobbyist that enjoys drawing up
-              PCBs and cases on my free time.
+              Most people on the internet know me as MelonBred. I&apos;m a
+              mechanical keyboard hobbyist that enjoys drawing up PCBs and cases
+              on my free time.
             </p>
           </div>
         </div>
