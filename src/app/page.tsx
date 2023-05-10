280d3d2ef90ad6c13bb320e43ce9947b65aff5ce
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-10 00:15:03 -0700

    Created menu button programmatically. Yup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,20 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 import GenericButton from "@/components/GenericButton";
+import MenuButton from "@/components/menu";
+
+import fs from "fs";
+import { dirname } from "path";
 
 export default function Home() {
+  const files = fs.readdirSync("./src/app/(content)", { withFileTypes: true });
+  let menuButtonRoutes = files
+    .filter((file) => file.isDirectory())
+    .map((file) => file.name);
+  console.log(menuButtonRoutes);
+
   return (
-    <div className="flex justify-center h-screen items-center bg-slate-800">
+    <div className="flex justify-center h-screen pt-20 bg-slate-800">
       <div className="flex flex-col items-center">
         <div className="md:flex md:justify-center m-4 w-max">
           <div className="flex justify-center">
@@ -29,6 +39,7 @@ export default function Home() {
             </p>
           </div>
         </div>
+        <MenuButton folders={menuButtonRoutes} />
         <GenericButton
           text="GitHub"
           href="https://github.com/melonbred"
---
+++ src/components/menu.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+type MenuProps = {
+  folders: string[];
+};
+
+export default function MenuButton(props: MenuProps) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <div className="bg-green-300 border-1 border-emerald-700 rounded-3xl">
+      <button onClick={() => setIsClicked(!isClicked)}>
+        <div className="bg-green-300 border-2 border-emerald-700 text-green-950 rounded-3xl w-80 h-16 flex items-center m-0 transition duration-150 hover:scale-105">
+          <div className="pl-4 pr-2">
+            <Image
+              alt={"Menu"}
+              height={30}
+              width={30}
+              src={"/iconmonstr-instagram-11.svg"}
+            />
+          </div>
+          <div className="flex w-52 justify-center font-mono tracking-tighter">
+            Menu
+          </div>
+        </div>
+      </button>
+
+      <div className="flex flex-col justify-center items-center bg-green-300 text-green-950 w-80 rounded-b-3xl">
+        {isClicked &&
+          props.folders.map((folders) => {
+            return <div>{folders}</div>;
+          })}
+      </div>
+    </div>
+  );
+}
