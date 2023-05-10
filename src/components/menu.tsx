2b86c34faeb84646659c2021e4e396d3e2dbc7d0
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-10 00:22:45 -0700

    Fixed scale with onCick.

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -13,7 +13,11 @@ export default function MenuButton(props: MenuProps) {
   return (
     <div className="bg-green-300 border-1 border-emerald-700 rounded-3xl">
       <button onClick={() => setIsClicked(!isClicked)}>
-        <div className="bg-green-300 border-2 border-emerald-700 text-green-950 rounded-3xl w-80 h-16 flex items-center m-0 transition duration-150 hover:scale-105">
+        <div
+          className={`${
+            isClicked ? "" : "hover:scale-105"
+          } bg-green-300 border-2 border-emerald-700 text-green-950 rounded-3xl w-80 h-16 flex items-center m-0 transition duration-150`}
+        >
           <div className="pl-4 pr-2">
             <Image
               alt={"Menu"}
@@ -31,7 +35,7 @@ export default function MenuButton(props: MenuProps) {
       <div className="flex flex-col justify-center items-center bg-green-300 text-green-950 w-80 rounded-b-3xl">
         {isClicked &&
           props.folders.map((folders) => {
-            return <div>{folders}</div>;
+            return <div className="hover:scale-110">{folders}</div>;
           })}
       </div>
     </div>
