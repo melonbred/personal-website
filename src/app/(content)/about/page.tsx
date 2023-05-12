63dd47676a73bfb5f308a3f01d3a9c8e1ea2a76e
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-12 15:33:51 -0700

    Fixed apostrophes in About page.

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -4,36 +4,36 @@ export default function About() {
       <h1 className="text-3xl font-bold pt-4">About Me</h1>
       <div className=" p-5 max-w-2xl">
         <p className="pt-5">
-          Located in the San Francisco, Bay Area, I am an engineer by day with a
-          love for tinkering and learning. Some other hobbies of mine include
-          skateboarding, snowboarding/skiing, photography, and all things
-          outdoors.
+          Located in the San Francisco, Bay Area, I&apos;m an engineer by day
+          with a love for tinkering and learning. Some other hobbies of mine
+          include skateboarding, snowboarding/skiing, photography, and all
+          things outdoors.
         </p>
         <p className="pt-5">
           My mechanical keyboard journey started around early 2018. Like many
           others, I found the mechanical keyboards subreddit and stumbled
-          quickly down the rabbit hole. It wasn't long until my eyes met with a
-          Daisy 40% keyboard and was instantly intrigued with the small form
-          factor and usability.
+          quickly down the rabbit hole. It wasn&apos;t long until my eyes met
+          with a Daisy 40% keyboard and was instantly intrigued with the small
+          form factor and usability.
         </p>
         <p className="pt-5">
           {" "}
           After a year into the hobby, the idea of tinkering and creating became
-          far more interesting that just consuming. So with the encouragement
+          far more interesting than just consuming. So with the encouragement
           from friends and the 40% keyboard community, I decided to give it a
           go.{" "}
         </p>
         <p className="pt-5">
-          Fast-forward to present day, and I've been fortunate enough to have
-          drawn up a handful of keyboards! With help from some dear friends and
-          wonderful community members, some of these creations have made it to
-          several hundred of people's desks and workstations.
+          Fast-forward to present day, and I&apos;ve been fortunate enough to
+          have drawn up a handful of keyboards! With help from some dear friends
+          and wonderful community members, some of these creations have made it
+          to several hundred of people's desks and workstations.
         </p>
         <p className="pt-5">
           If you have supported my projects, thank you from the bottom of my
           heart. If you are finding them the first time, welcome! I hope to keep
-          working on projects as ideas strike, and you'll likely find them here
-          on this site.
+          working on projects as ideas strike, and you&apos;ll likely find them
+          here on this site.
         </p>
       </div>
     </div>
