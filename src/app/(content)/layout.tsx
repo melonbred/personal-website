6b6d3a11831c38123c4a5e614a09a011dc6dedd8
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-13 16:39:50 -0700

    Changed a tag to Home to next/Link tag.

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,5 +1,6 @@
 import "../globals.css";
 import Image from "next/image";
+import Link from "next/link";
 import MenuButton from "@/components/menu";
 
 export const metadata = {
@@ -15,12 +16,12 @@ export default function RootLayout({
   return (
     <div className="bg-slate-800 min-h-screen">
       <div className="flex justify-between items-start fixed top-0 left-3 right-3">
-        <a
+        <Link
           className="rounded-full bg-green-300 border-4 border-emerald-700 mt-2 hover:scale-105 duration-150"
           href="/"
         >
           <Image alt="turtle" width="80" height="80" src="/turtlepan.png" />
-        </a>
+        </Link>
         <MenuButton isHome={false} addTW="mt-4" />
       </div>
       {children}
