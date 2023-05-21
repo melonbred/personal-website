51cc925d6dc7f1a3b6d34137ee2f746ee24648f1
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-21 11:51:13 -0700

    Revised keyboardCard to be responsive between small and medium resolutions. Still saved in Resources page as a WIP.

--- src/components/KeyboardCard.tsx
+++ src/components/KeyboardCard.tsx
@@ -17,15 +17,15 @@ export default function KeyboardCard({
   return (
     <Link
       href={href}
-      className="m-1 flex w-80 flex-col rounded-3xl bg-green-300 text-green-950 transition duration-150 hover:scale-[1.043] md:w-[360px] "
+      className="m-1 flex w-80 flex-col rounded-3xl bg-green-300 text-green-950 md:w-[800px] md:flex-row "
     >
       <img
-        className="m-0 h-[239.76px] w-full overflow-hidden rounded-3xl border-4 border-emerald-700 bg-green-300 object-cover object-center"
+        className="m-0 h-[239.76px] w-full overflow-hidden rounded-3xl border-4 border-emerald-700 bg-green-300 object-cover object-center md:h-[350px]"
         alt={title}
         src={img}
       ></img>
-      <div className="flex flex-col items-start p-4">
-        <h2 className="text-lg">{title}</h2>
+      <div className="flex flex-col items-start justify-center p-4 md:w-72">
+        <h2 className="text-xl">{title}</h2>
         <p className="text-left">{body}</p>
       </div>
     </Link>
