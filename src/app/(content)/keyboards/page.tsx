2ba7b735537fc8d99006aff0108bbfe6427c1350
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-22 00:02:44 -0700

    Revised KeyboardCard component so Link target is only More! button and not entire card. Also updated Keyboard Card component colors.

--- src/app/(content)/keyboards/page.tsx
+++ src/app/(content)/keyboards/page.tsx
@@ -17,7 +17,7 @@ export default function Resources() {
       <div className="min-w-screen flex flex-col items-center pb-5 pt-24">
         <h1 className="pt-4 text-3xl">Keyboards</h1>
         <div className="min-w-screen p-4 md:p-10">
-          <h2 className="pt-5 text-2xl">MB Series</h2>
+          <h2 className="pt-1 text-2xl">MB Series</h2>
           <p className="pt-2">
             Projects in the MB series all started off as personal experiments
             with the intent to learn and try different things. These projects
--- src/components/KeyboardCard.tsx
+++ src/components/KeyboardCard.tsx
@@ -15,23 +15,25 @@ export default function KeyboardCard({
   img,
 }: KeyboardCardProps) {
   return (
-    <Link
-      href={href}
-      className="m-1 flex w-80 flex-col rounded-3xl bg-green-300 text-green-950 md:w-[800px] md:flex-row "
-    >
+    <div className="m-1 flex w-80 flex-col rounded-3xl bg-cyan-300 text-cyan-950 md:w-[800px] md:flex-row ">
       <img
-        className="m-0 h-[239.76px] w-full overflow-hidden rounded-3xl border-4 border-emerald-700 bg-green-300 object-cover object-center md:h-[350px]"
+        className="m-0 h-[239.76px] w-full overflow-hidden rounded-3xl border-4 border-sky-700 bg-cyan-300 object-cover object-center md:h-[400px]"
         alt={title}
         src={img}
       ></img>
       <div className="flex flex-col items-start justify-center p-4 md:w-72">
         <h2 className="text-xl">{title}</h2>
-        <p className="text-left">{body}</p>
-        <div className="mt-auto flex w-full items-center justify-end pt-4">
-          More!
-          <ChevronDoubleRightIcon className="h-5 w-5 "></ChevronDoubleRightIcon>
+        <p className="pt-1 text-left">{body}</p>
+        <div className="mt-auto flex w-full items-center justify-end rounded-md pt-4">
+          <Link
+            href={href}
+            className="flex items-center rounded-2xl border-4 border-emerald-700 bg-green-300 p-1 transition duration-150 hover:scale-110"
+          >
+            More!
+            <ChevronDoubleRightIcon className="h-5 w-5 "></ChevronDoubleRightIcon>
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
