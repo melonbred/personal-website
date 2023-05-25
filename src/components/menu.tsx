35b3ac7d929ef89868a60be041a281b3a969fdcb
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-25 00:35:54 -0700

    Added click event listener to close menu component when clicked in document body.

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -10,54 +10,57 @@ type MenuProps = {
 };
 
 export default function MenuButton(props: MenuProps) {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const menuArray = ["About", "Keyboards", "Resources", "Contact"];
 
   return (
-    <div
-      className={`${props.addTW ? props.addTW : ""} 
+    <>
+      {document.body.addEventListener("click", () => setIsOpen(false))}
+      <div
+        className={`${props.addTW ? props.addTW : ""} 
       ${
         props.isHome ? "" : "absolute right-3"
       } m-1 rounded-3xl border-emerald-700 bg-green-300`}
-    >
-      <button onClick={() => setIsClicked(!isClicked)}>
-        <div
-          className={`${isClicked ? "" : "hover:scale-105"}
+      >
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <div
+            className={`${isOpen ? "" : "hover:scale-105"}
           ${props.isHome ? "w-80" : "w-40 md:w-80"}
            m-0 flex h-16 items-center rounded-3xl border-4 border-emerald-700 bg-green-300 text-green-950 duration-150`}
-        >
-          <Bars3Icon className="ml-4 mr-2 h-8 w-8"></Bars3Icon>
+          >
+            <Bars3Icon className="ml-4 mr-2 h-8 w-8"></Bars3Icon>
 
-          <div className="flex w-52 justify-center">Menu</div>
-        </div>
-      </button>
+            <div className="flex w-52 justify-center">Menu</div>
+          </div>
+        </button>
 
-      <div
-        className={`${
-          isClicked
-            ? "translate-y-0 transition-transform duration-200"
-            : "-translate-y-20 scale-y-0"
-        } 
+        <div
+          className={`${
+            isOpen
+              ? "translate-y-0 transition-transform duration-200"
+              : "-translate-y-20 scale-y-0"
+          } 
         ${props.isHome ? "w-80" : "w-40 md:w-80"}
         relative flex flex-col items-center justify-center rounded-b-3xl bg-green-300 text-green-950`}
-      >
-        {isClicked &&
-          menuArray.map((folder) => {
-            return (
-              <div
-                key={folder}
-                className="m-1 flex w-32 justify-center rounded-3xl transition duration-100 hover:scale-110 hover:bg-green-400"
-              >
-                <Link
-                  onClick={() => setIsClicked(false)}
-                  href={`/${folder.toLocaleLowerCase()}`}
+        >
+          {isOpen &&
+            menuArray.map((folder) => {
+              return (
+                <div
+                  key={folder}
+                  className="m-1 flex w-32 justify-center rounded-3xl transition duration-100 hover:scale-110 hover:bg-green-400"
                 >
-                  {folder}
-                </Link>
-              </div>
-            );
-          })}
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    href={`/${folder.toLocaleLowerCase()}`}
+                  >
+                    {folder}
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
