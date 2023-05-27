701f72e0dd0731921f065c43efae2193d4d67aa2
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-27 11:36:35 -0700

    Updated About page.

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function About() {
   return (
     <div className="flex min-w-min flex-col items-center pb-5 pt-24">
@@ -10,36 +12,30 @@ export default function About() {
           things outdoors.
         </p>
         <p className="pt-5">
-          My mechanical keyboard journey started around early 2018. Like many
-          others, I found the mechanical keyboards subreddit and stumbled
-          quickly down the rabbit hole. It wasn&apos;t long until my eyes met
-          with a Daisy 40% keyboard and was instantly intrigued with the small
-          form factor and usability.
-        </p>
-        <p className="pt-5">
-          {" "}
-          After a year into the hobby, the idea of tinkering and creating became
-          far more interesting than just consuming. So with the encouragement
-          from friends and the 40% keyboard community, I decided to give it a
-          go.{" "}
+          My mechanical keyboard journey started around early 2018. After a year
+          into the hobby, the idea of tinkering and creating became far more
+          interesting than just consuming. So with the encouragement from
+          friends in the community, I decided to give it a go.{" "}
         </p>
         <p className="pt-5">
           Fast-forward to present day, and I&apos;ve been fortunate enough to
           have drawn up a handful of keyboards! With help from some dear friends
           and wonderful community members, some of these creations have made it
-          to several hundreds of people&apos;s desks and workstations.
-        </p>
-        <p className="pt-5">
-          If you have supported my projects, thank you from the bottom of my
-          heart.
-        </p>
-        <p className="pt-5">
-          If you are finding them for the first time, welcome!
+          to several hundred desks and workstations.
         </p>
         <p className="pt-5">
           I hope to keep working on projects as ideas strike, and you&apos;ll
           likely find them here on this site.
         </p>
+        <div className="flex items-center justify-end">
+          <p className="p-5">Thanks for visiting!</p>
+          <Image
+            src="./images/misc/turtlepan_white.svg"
+            width="75"
+            height="75"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
