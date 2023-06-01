60af79cfa441249394bed5faf2117179c55a06f4
melonbred <61572569+melonbred@users.noreply.github.com>
2023-05-31 22:46:25 -0700

    Initialized branch. Start of keyboard pages content with Griffin page.

--- src/app/(content)/keyboards/pegasus/page.tsx
+++ src/app/(content)/keyboards/pegasus/page.tsx
@@ -4,7 +4,30 @@ export default function Pegasus() {
       <div className="min-w-screen flex flex-col items-center pb-5 pt-24">
         <h1 className="pt-4 text-3xl">Pegasus</h1>
         <div className="min-w-screen p-4 md:p-10">
-          <h2 className="pt-5 text-2xl">Work In Progress</h2>
+          <h2 className="pt-5 text-2xl">Overview</h2>
+          <p className="pt-2">
+            Pegasus is the first in the Mythics series keyboards. A 40% keyboard
+            with a 12.75u "WKL" layout and 0.75u blockers, this project was as
+            much as an experiment as all the projects that came before it. A
+            4-piece construction that offers a unique win keyless (WKL) layout,
+            Pegasus was a premium offering into the 40% keyboard space.
+          </p>
+          <h2 className="pt-10 text-2xl">Case Construction</h2>
+          <p className="pt-5">
+            The 4-piece construction includes components typical of many
+            keyboards: top, bottom, switch plate and weight. However, what is
+            unique about Pegasus is the plate mounting solution. The switch
+            plate is a half plate that is integrated into the raised bezel
+            commponent of the keyboard. This component is then sandwiched
+            between the case and bottom components, isolated by open-cell foam
+            strips. The idea was to provide a soft, flexible typing experience
+            in the alphas while offering a stable platform in the modifiers for
+            tap-hold functions.
+          </p>
+          <p className="pt-5">
+            The weight featured an engraving of a Pegasus and was offered in raw
+            brass and polished stainless steel
+          </p>
         </div>
       </div>
     </>
