a96ec07b7fbb487a10ddc1c6a639ec2519311ae4
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-07 10:56:32 -0700

    Added temporary text to Resources and Contact. Added Upcoming Group Buy section to ExternalLinks component.

--- src/components/ExternalLinks.tsx
+++ src/components/ExternalLinks.tsx
@@ -3,6 +3,12 @@ import GenericButton from "./GenericButton";
 export default function ExternalLinks() {
   return (
     <div className="flex flex-col items-center">
+      <div className="mt-6">Upcoming Group Buy</div>
+      <GenericButton
+        text="Shop Griffin 60%"
+        href="https://p3dstore.com/products/griffin-group-buy"
+        img="/icons/DocumentCheck.svg"
+      />
       <div className="mt-6">External Links</div>
       <GenericButton
         text="GitHub"
@@ -13,11 +19,6 @@ export default function ExternalLinks() {
         text="Instagram"
         href="https://www.instagram.com/melonbred.kb/"
         img="/icons/iconmonstr-instagram-11.svg"
-      />
-      <GenericButton
-        text="Griffin 60% Keyboard"
-        href="https://geekhack.org/index.php?topic=120200.0"
-        img="/icons/DocumentCheck.svg"
       />
     </div>
   );
