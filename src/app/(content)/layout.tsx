6d89ab2c761f3ec7703f02dd2fbe2aea23eaf338
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-06 19:00:12 -0700

    Added overflow scroll to sidebar on large resolution.

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -31,29 +31,31 @@ export default function RootLayout({
         </Link>
         <MenuButton isHome={false} addTailWind="mt-4 absolute right-3" />
       </div>
-      <div className="lg hidden lg:relative lg:top-0 lg:mr-2 lg:flex lg:h-full lg:flex-col lg:items-center lg:bg-slate-600 lg:p-3 ">
-        <div className="flex items-center">
-          <Link
-            className="rounded-full border-4 border-emerald-700 bg-green-300 duration-150 hover:scale-105 hover:bg-green-400"
-            href="/"
-          >
-            <Image
-              alt="turtle"
-              width="80"
-              height="80"
-              src="/images/misc/turtlepan.png"
-            />
-          </Link>
-          <div className="flex flex-col justify-start p-2">
-            <h1>Welcome!</h1>
-            <p>This is melonbred.dev.</p>
+      <aside className="hidden lg:relative lg:top-0 lg:mr-2 lg:flex lg:h-full lg:flex-col lg:items-center lg:bg-slate-600 lg:p-3 ">
+        <div className="overflow-x-hidden overflow-y-scroll">
+          <div className="flex items-center">
+            <Link
+              className="rounded-full border-4 border-emerald-700 bg-green-300 duration-150 hover:scale-105 hover:bg-green-400"
+              href="/"
+            >
+              <Image
+                alt="turtle"
+                width="80"
+                height="80"
+                src="/images/misc/turtlepan.png"
+              />
+            </Link>
+            <div className="flex flex-col justify-start p-2">
+              <h1>Welcome!</h1>
+              <p>This is melonbred.dev.</p>
+            </div>
           </div>
+          <div className="mt-10"></div>
+          <MenuLg />
+          <div className="mt-10"></div>
+          <ExternalLinks />
         </div>
-        <div className="mt-10"></div>
-        <MenuLg />
-        <div className="mt-10"></div>
-        <ExternalLinks />
-      </div>
+      </aside>
       <div className="overflow-y-auto rounded-3xl lg:bg-slate-800">
         {children}
       </div>
