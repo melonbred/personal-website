d060a0d3fd2cd84fc11bc5118a89afeba8613aba
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-08 16:45:41 +0900

    Added Griffin images provided by P3D. Added sliderJS header to Griffin Page.

--- src/app/(content)/keyboards/griffin/page.tsx
+++ src/app/(content)/keyboards/griffin/page.tsx
@@ -1,18 +1,72 @@
+"use client";
+
 import Image from "next/image";
 
-export default function MB44() {
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "@/app/swiper.css";
+import "swiper/css/pagination";
+
+export default function Griffin() {
   return (
     <>
-      <div className="flex flex-col items-center pb-5 lg:bg-slate-800">
-        <div className="h-24 lg:hidden"></div>
-        <img
-          alt="griffin side"
-          src="/images/keyboards/griffin/griffin_btm.png"
-          width="auto"
-          height="auto"
-          className="rounded-3xl object-cover p-3"
-        />
-
+      <div className="h-24 lg:hidden"></div>
+      <div className="p-3">
+        <Swiper
+          centeredSlides={true}
+          modules={[Navigation, Pagination, A11y]}
+          navigation={true}
+          pagination={{ clickable: true }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <div className="relative h-60 md:h-[500px] lg:h-[650px]">
+              <Image
+                alt="griffin btm"
+                src="/images/keyboards/griffin/griffin_btm.png"
+                fill={true}
+                className="rounded-2xl object-cover"
+              />
+              <p className="absolute bottom-1 left-2 text-sm text-black">
+                Provided by P3D
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative h-60 md:h-[500px] lg:h-[650px]">
+              <Image
+                alt="griffin top"
+                src="/images/keyboards/griffin/griffin_top.png"
+                fill={true}
+                className="rounded-2xl object-cover"
+              />
+              <p className="absolute bottom-1 left-2 text-sm text-black">
+                Provided by P3D
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="relative h-60 md:h-[500px] lg:h-[650px]">
+              <Image
+                alt="griffin profile"
+                src="/images/keyboards/griffin/griffin_profile.png"
+                fill={true}
+                className="rounded-2xl object-cover"
+              />
+              <p className="absolute bottom-1 left-2 text-sm text-black">
+                Provided by P3D
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="flex flex-col items-center pb-5">
         <h1 className="pt-4 text-3xl">Griffin</h1>
         <div className="p-4 md:p-10">
           <h2 className="pt-5 text-2xl">Overview</h2>
@@ -131,16 +185,16 @@ export default function MB44() {
               <h2 className="pt-10 text-2xl">Timeline</h2>
               <p className="pt-5">
                 P3D Store is the worldwide vendor for Griffin.{" "}
-                <ul className="ml-4 list-disc">
-                  <li className="pt-5">
-                    Group buy is to run from July 1st through August 1st, 2023.
-                  </li>
-                  {/* <li className="pt-5">
+              </p>
+              <ul className="ml-4 list-disc">
+                <li className="pt-5">
+                  Group buy is to run from July 1st through August 1st, 2023.
+                </li>
+                {/* <li className="pt-5">
                   Group buy was fulfilled on January 1st, 2023.
                 </li>
                 <li className="pt-5">Extras sold on January 25th, 2023.</li> */}
-                </ul>
-              </p>
+              </ul>
             </div>
           </div>
         </div>
