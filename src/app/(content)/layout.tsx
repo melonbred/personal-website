010c3f1236cc83c167b601fb2d290d37d144222f
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-07 18:55:51 -0700

    Updated persistent sidebar width to 335px to allow for button scaling on hover.

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -31,9 +31,10 @@ export default function RootLayout({
         </Link>
         <MenuButton isHome={false} addTailWind="mt-4 absolute right-3" />
       </div>
+      {/* persistent sidebar for large resolution */}
       <aside className="hidden lg:relative lg:top-0 lg:mr-2 lg:flex lg:h-full lg:flex-col lg:items-center lg:bg-slate-600 lg:p-5 ">
         <div className="overflow-x-hidden overflow-y-scroll">
-          <div className="flex h-full flex-col justify-between">
+          <div className="flex h-full w-[335px] flex-col items-center justify-between">
             <div className="flex items-center">
               <Link
                 className="rounded-full border-4 border-emerald-700 bg-green-300 duration-150 hover:scale-105 hover:bg-green-400"
