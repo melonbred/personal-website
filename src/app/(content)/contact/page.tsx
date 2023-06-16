152aecdebb0d11004a1867fbcd31ee350cb0083f
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-16 23:20:36 +0900

    Added email information on Contact page.

--- src/app/(content)/contact/page.tsx
+++ src/app/(content)/contact/page.tsx
@@ -1,37 +1,24 @@
+import Link from "next/link";
+
 export default function Contact() {
   return (
     <>
       <div className="min-w-screen flex flex-col items-center pb-5 pt-24 lg:pt-10">
         <h1 className="pt-4 text-3xl">Contact</h1>
         <div className="flex flex-col items-center p-4 md:p-10">
-          <h2 className="pt-5 text-2xl">Work In Progress</h2>
-          <p className="p-10">But here is some text to chew on.</p>
-          <p className="">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec
-            feugiat in fermentum posuere urna nec tincidunt. Magna sit amet
-            purus gravida quis blandit turpis. Orci dapibus ultrices in iaculis.
-            Volutpat est velit egestas dui id ornare. Bibendum arcu vitae
-            elementum curabitur vitae. Bibendum at varius vel pharetra.
-            Vestibulum lectus mauris ultrices eros. Sit amet purus gravida quis
-            blandit turpis. Faucibus a pellentesque sit amet porttitor eget.
-            Scelerisque felis imperdiet proin fermentum leo vel. Vitae suscipit
-            tellus mauris a diam maecenas. Ullamcorper eget nulla facilisi etiam
-            dignissim. Nisl pretium fusce id velit ut tortor pretium viverra
-            suspendisse. Viverra maecenas accumsan lacus vel facilisis volutpat.
-            Consectetur adipiscing elit pellentesque habitant morbi tristique
-            senectus. Elementum sagittis vitae et leo duis ut diam quam nulla.
-            Senectus et netus et malesuada fames ac turpis egestas maecenas.
-            Quam pellentesque nec nam aliquam sem. Morbi blandit cursus risus at
-            ultrices mi tempus imperdiet. Blandit libero volutpat sed cras
-            ornare. Purus in massa tempor nec. Vel eros donec ac odio tempor.
-            Vitae aliquet nec ullamcorper sit. Pretium fusce id velit ut tortor
-            pretium viverra suspendisse. Eu consequat ac felis donec et odio.
-            Sapien pellentesque habitant morbi tristique senectus et netus. Sit
-            amet volutpat consequat mauris nunc congue nisi vitae. Amet cursus
-            sit amet dictum. Auctor elit sed vulputate mi. Purus semper eget
-            duis at.
+          <p className="p-10">
+            If you have any questions about any projects that I have worked on,
+            or would like to collaborate on a project in the future, please feel
+            free to shoot me a message at{" "}
+            <Link
+              href="mailto:contact@melonbred.dev"
+              className="underline hover:decoration-emerald-400/80 hover:decoration-4"
+            >
+              contact@melonbred.dev
+            </Link>
+            .
           </p>
+          <p className=""></p>
         </div>
       </div>
     </>
