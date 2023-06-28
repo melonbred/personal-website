7db86880fc608388833fb804e50714c4c15fac51
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-27 23:19:39 -0700

    KeyboardSpecTable component to abstract table style from keyboard pages. Keyboard Pages updated to use KeyboardSpecTable component.

--- src/app/(content)/keyboards/pegasus/page.tsx
+++ src/app/(content)/keyboards/pegasus/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import ImageCard from "@/components/ImageCard";
+import KeyboardSpecTable from "@/components/KeyboardSpecTable";
 
 // import Swiper core and required modules
 import { Navigation, Pagination, A11y } from "swiper";
@@ -101,54 +102,15 @@ export default function Pegasus() {
           <div className="flex flex-col md:flex-row">
             <div className="md:w-1/2">
               <h2 className="pb-5 pt-10 text-2xl">Specifications</h2>
-              <div className="relative overflow-x-auto rounded-xl">
-                <table className="w-full">
-                  <tbody>
-                    <tr className="bg-slate-600">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Layout
-                      </th>
-                      <td>40% - 12.75u WKL</td>
-                    </tr>
-                    <tr className="bg-slate-500">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Plate Mounting
-                      </th>
-                      <td>Gasket Mounted Integrated Half-Plate</td>
-                    </tr>
-                    <tr className="bg-slate-600">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Typing Angle
-                      </th>
-                      <td>6 degrees</td>
-                    </tr>
-                    <tr className="bg-slate-500">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Front Height
-                      </th>
-                      <td>20 mm</td>
-                    </tr>
-                    <tr className="bg-slate-600">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Effective Key Height
-                      </th>
-                      <td>24.2 mm</td>
-                    </tr>
-                    <tr className="bg-slate-500">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Case Material
-                      </th>
-                      <td>6063 Aluminium</td>
-                    </tr>
-                    <tr className="bg-slate-600">
-                      <th scope="row" className="px-5 py-3 text-left uppercase">
-                        Weight Material
-                      </th>
-                      <td>Brass or Polished Stainless Steel</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <KeyboardSpecTable
+                layout="40% - 12.75u WKL"
+                mounting="Gasket Mounted Integrated Half-Plate"
+                angle="6 degrees"
+                height="20 mm"
+                ekh="24.2 mm"
+                caseMaterial="6063 Aluminium"
+                weightMaterial="Brass or Polished Stainless Steel"
+              />
             </div>
             <div className="md:w-1/2 md:pl-10">
               <h2 className="pt-10 text-2xl">Timeline</h2>
