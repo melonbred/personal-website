edadd8f3c6287b332362ddf01ecea60fdc8ff732
melonbred <61572569+melonbred@users.noreply.github.com>
2023-06-27 21:50:43 -0700

    tailwind.config.js updated to add screen resolutions. layout updated for larger screen resolutions.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xl: "1280px",
+        "2xl": "1800px",
+        "3xl": "2600px",
+      },
     },
   },
   plugins: [],
