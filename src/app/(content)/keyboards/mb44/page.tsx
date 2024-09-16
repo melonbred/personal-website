5172343c7c7721304ecf023acea194af9025dc6c
melonbred <61572569+melonbred@users.noreply.github.com>
2024-09-16 13:56:25 -0700

    Generate static OG iamge on MB44 page.

--- src/app/(content)/keyboards/mb44/page.tsx
+++ src/app/(content)/keyboards/mb44/page.tsx
@@ -25,10 +25,7 @@ export default function MB44() {
           content="MB-44! A 40% mechanical keyboard by MelonBred, this is the first in the MB line of boards that offers a compact 40% layout and a fun 65%-esque blocker."
         />
         <meta property="og:title" content="MB-44 Keyboard" />
-        <meta
-          property="og: image"
-          content="https://melonbred.dev/images/mb_og.png"
-        />
+        <meta property="og: image" content="<generated>" />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="300" />
